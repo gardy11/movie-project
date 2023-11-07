15e7f422186c1f2b3fb5901970bd1984f27fd203
gardy11 <106308847+gardy11@users.noreply.github.com>
2023-11-07 21:34:33 +0800

    change

--- src/components/front/PopularMovies.vue
+++ src/components/front/PopularMovies.vue
@@ -1,44 +1,53 @@
 <template>
   <div class="mx-5">
-    <h2 class="mt-5 text-xl font-semibold text-yellow-500">
-        近期受歡迎
-    </h2>
+    <h2 class="mt-5 text-xl font-semibold text-yellow-500">近期受歡迎</h2>
 
-    <div class="grid grid-cols-1 gap-8 mt-6 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4">
-      <MovieItem v-for="movie in movies" :key="movie.id" :movie="movie" :genres="genres" />
+    <div
+      class="grid grid-cols-2 gap-8 mt-6 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4"
+    >
+      <MovieItem
+        v-for="movie in movies"
+        :key="movie.id"
+        :movie="movie"
+        :genres="genres"
+      />
     </div>
-  </div> 
+  </div>
 </template>
 
 <script>
 import MovieItem from "../items/MovieItem";
 
 export default {
-    components: {
-        MovieItem,
-    },
+  components: {
+    MovieItem,
+  },
 
-    data: function(){
-      return {
-        movies: [],
-        genres: [],
-      };
-    },
-   
-    async mounted() {
-      this.fetchGenres();
-      try {
-        const response = await this.$http.get("/movie/popular?language=zh-TW&region=TW");
-        this.movies = response.data.results;
-      }catch(error){
-        console.log(error);
-      }
-    },
+  data: function () {
+    return {
+      movies: [],
+      genres: [],
+    };
+  },
 
-    methods: {
+  async mounted() {
+    this.fetchGenres();
+    try {
+      const response = await this.$http.get(
+        "/movie/popular?language=zh-TW&region=TW"
+      );
+      this.movies = response.data.results;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  methods: {
     async fetchGenres() {
       try {
-        const response = await this.$http.get("/genre/movie/list?language=zh-TW");
+        const response = await this.$http.get(
+          "/genre/movie/list?language=zh-TW"
+        );
         this.genres = response.data.genres;
       } catch (error) {
         console.log(error);
@@ -49,5 +58,4 @@ export default {
 </script>
 
 <style>
-
 </style>
